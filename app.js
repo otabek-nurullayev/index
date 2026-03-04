b0068e40b5c169855709c2f338986ebbe195d4c1
otabek-nurullayev <otabeknurulayev093@gmail.com>
2026-03-04 19:13:40 +0500

    asdfghj

--- app.js
+++ app.js
@@ -1,7 +1,83 @@
 'use strict'
-let ism ='Otabek';
-console.log(ism);
-let familya='Nurullayev';
-console.log(familya);
-let yosh='18';
-console.log(yosh)
+//let ism ='Otabek';
+//console.log(ism);
+//let familya='Nurullayev';
+//console.log(familya);
+//let yosh='18';
+//console.log(yosh)
+//let a = 10,b = 8;
+//console.log(a+b);
+//console.log(a-b);
+//console.log(a*b);
+//console.log(a/b);
+//console.log(a%b);
+//console.log(a++);
+//console.log(++a);
+//console.log(b++);
+//console.log(++b);
+//console.log(b);
+//console.log(a);
+//let a = 5 ,b = 10 ,c = "5";
+//const  z = a;
+//console.log(a==b);
+//console.log(a===b);
+//console.log(a==z);
+//console.log(a===z);
+//console.log(a!=b);
+//console.log(a!=z);
+//console.log(a>b);
+//console.log(a>z);
+//console.log(a>=b);
+//console.log(a<=b);
+//let a = 10, b = 15;
+//console.log(a);
+//a = b;
+//console.log(a);
+//a += b;
+//console.log(a);
+//a -= b;
+//console.log(a);
+//a *=b;
+//console.log(a);
+//a /= b;
+//console.log(a);
+//a %= b;
+//console.log(a);
+//let ism = "Otabek";
+//let familyasi = ("Salom " + ism +" !");
+//console.log(familyasi);
+//let ism = "Otabek";
+//let familya = `Salom ${ism} !`;
+//console.log(familya)
+//const htmlni_tugatgan = true;
+//const cssni_tugatgan = true;
+//
+//let xabarberadi = ' ';
+//if (htmlni_tugatgan && cssni_tugatgan) {
+//    xabarberadi = ' kursni boshlashingiz mumkin';
+//}
+//else if (htmlni_tugatgan || cssni_tugatgan) {
+//        xabarberadi = ' kursni boshlay olmaysiz';
+//
+//} 
+//else {
+//            xabarberadi = ' kursni boshlay olmaysiz';
+//
+//}
+//console.log(xabarberadi);
+// const yosh = '18';
+// console.log(yosh + 1);
+// console.log(Number(yosh)+1);
+// const yosh = 'Otabek';
+// console.log(yosh);
+// console.log(Number(yosh));
+// const yosh = '1234567890';  
+// console.log(yosh);
+// console.log(String(yosh));
+// const ism = ' ';
+// if (ism) {
+//     console.log("ismingizni kiriting!"+ism);
+// } 
+// else {
+//     console.log("szning ismingiz");
+// }
